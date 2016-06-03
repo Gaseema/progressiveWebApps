9e6ef5e93aa71f87232f20ab00cfefccba77a918
Gaseema Ndung'u <gaseema.n@gmail.com>
2016-06-03 13:17:37 +0300

    used service workers to pre-cashe the app shell

--- scripts/app.js
+++ scripts/app.js
@@ -294,4 +294,12 @@
         app.saveSelectedCities();
     }
 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () {
+                console.log('Service Worker Registered');
+            });
+    }
+
 })();
